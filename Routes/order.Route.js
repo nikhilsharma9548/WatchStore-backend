4780e0d3d52e5f719e297f2d24941178cc7a7d45
nikhilsharma9548 <ns630403@gmail.com>
2025-09-02 00:19:23 +0530

    soo

--- Routes/order.Route.js
+++ Routes/order.Route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import userAuth from '../MiddleWare/authUser.middleware.js'
 import { getAllOrder, getUserOrder, placeOrderCOD } from '../Controllers/order.controller.js'
-import authAdmin from '../MiddleWare/AuthAdmin.middleware.js'
+import authAdmin from '../MiddleWare/authadmin.middleware.js'
 
 const orderRouter = express.Router()
 
