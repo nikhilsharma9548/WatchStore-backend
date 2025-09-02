c33c72464ed70bfd0fa9bb96d1d0ef87c8d736bb
nikhilsharma9548 <ns630403@gmail.com>
2025-09-02 23:51:15 +0530

    fine

--- Routes/user.Route.js
+++ Routes/user.Route.js
@@ -1,6 +1,7 @@
 import express from "express" 
-import { isAuth, login, logout, register } from "../Controllers/user.controller.js";
+import { isAuth, login, logout, register, uploadProfileImage } from "../Controllers/user.controller.js";
 import userAuth from "../MiddleWare/authUser.middleware.js";
+import { upload } from "../Configs/Multer.js";
 
 const userRouter = express.Router();
 
@@ -8,7 +9,7 @@ const userRouter = express.Router();
 userRouter.post('/register', register)
 userRouter.post('/login', login)
 userRouter.get('/is-auth',userAuth, isAuth)
-router.post("/upload-profile", userAuth, upload.single("image"), uploadProfileImage);
+userRouter.post("/upload-profile", userAuth, upload.single("image"), uploadProfileImage);
 userRouter.post('/logout', logout)
 
 
