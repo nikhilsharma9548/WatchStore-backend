242f31206b2eff3cda7d06c0613f30935b41cb7f
nikhilsharma9548 <ns630403@gmail.com>
2025-09-02 09:32:30 +0530

    sure

--- MiddleWare/authUser.middleware.js
+++ MiddleWare/authUser.middleware.js
@@ -1,29 +1,51 @@
 import jwt from "jsonwebtoken";
 
-const userAuth = async (req, res, next) => {
+// const userAuth = async (req, res, next) => {
     
-    const { token } = req.cookies;
+//     const { token } = req.cookies;
 
-    if (!token) {
-        return res.json({ success: false, message: "Not authorized user, login again" });
+//     if (!token) {
+//         return res.json({ success: false, message: "Not authorized user, login again" });
+//     }
+
+//     try {
+//         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+//         if (tokenDecode.id) {
+//             if (!req.body) req.body = {}; // ✅ IMPORTANT LINE
+//             req.body.userId = tokenDecode.id;
+//             next();
+
+//             console.log("JWT_SECRET:", process.env.JWT_SECRET)
+//         } else {
+//             return res.json({ success: false, message: "Not authorized. Please login again" });
+//         }
+
+//     } catch (error) {
+//         return res.json({ success: false, message: "Data not found" });
+//     }
+// };
+
+// export default userAuth;
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.json({ success: false, message: "Not authorized user, login again" });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (tokenDecode.id) {
+      req.userId = tokenDecode.id; // ✅ yaha body ke bajaye direct userId attach kar
+      next();
+    } else {
+      return res.json({ success: false, message: "Not authorized. Please login again" });
     }
 
-    try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-
-        if (tokenDecode.id) {
-            if (!req.body) req.body = {}; // ✅ IMPORTANT LINE
-            req.body.userId = tokenDecode.id;
-            next();
-
-            console.log("JWT_SECRET:", process.env.JWT_SECRET)
-        } else {
-            return res.json({ success: false, message: "Not authorized. Please login again" });
-        }
-
-    } catch (error) {
-        return res.json({ success: false, message: "Data not found" });
-    }
+  } catch (error) {
+    return res.json({ success: false, message: "Data not found" });
+  }
 };
-
-export default userAuth;
