3e9a434eae827799dd4272063efe8fc2c46fa29d
nikhilsharma9548 <ns630403@gmail.com>
2025-09-03 23:40:32 +0530

    ok

--- Controllers/address.controller.js
+++ Controllers/address.controller.js
@@ -22,7 +22,7 @@ export const getAddress = async(req, res) =>{
     try {
         const { userId } = req.body
         const addresses = await Address.find({userId})
-        res.json({success: true, addresses, message: "address not found! please add address"})
+        res.json({success: true, addresses})
     } catch (error) {
          console.log(error.message)
          res.json({success:false, message:error.message })
