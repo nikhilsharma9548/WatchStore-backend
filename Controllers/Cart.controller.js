a37aeba733e4bdf9ed00fbaa9f6f374175da3f0c
nikhilsharma9548 <ns630403@gmail.com>
2025-09-17 20:44:33 +0530

    ok

--- Controllers/Cart.controller.js
+++ Controllers/Cart.controller.js
@@ -6,12 +6,12 @@ export const updateCart =async(req, res) =>{
     try {
         
         const { userId, cartItems } = req.body
-
+        
         await User.findByIdAndUpdate(userId, {cartItems})
         res.json({success: true, message: "Cart updated"})
 
     } catch (error) {
         console.log(error.message)
-        res.json({success: false, message: "ewwww" || "Internal server error"})
+        res.json({success: false, message: error.message || "Internal server error"})
     }
 }
